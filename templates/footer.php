5016de485783d7566de11c060405485ca0821270
MatheusZuchiBalbinot <104937774+MatheusZuchiBalbinot@users.noreply.github.com>
2023-02-16 21:52:37 -0300

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,45 @@
+<footer>
+
+<ul id="customer_service">
+
+    <li id="subtitle"> ATENDIMENTO AO CLIENTE </li>
+    <li> Central de Ajuda</li>
+    <li> Como Comprar</li>
+    <li> Métodos de Pagamento</li>
+    <li> Sobre Garantias</li>
+    <li> Devoluções e Reembolso</li>
+    <li> Contate-nos</li>
+    <li> Reclamações </li>
+
+</ul>
+
+<ul id="about_discontada"> 
+
+    <li id="subtitle"> SOBRE A DISCONTADA </li>
+    <li> Sobre nós</li>
+    <li> Nossas Políticas</li>
+    <li> Política de Privacidade</li>
+    <li> Ofertas Relâmpago</li>
+    <li> Devoluções e Reembolso</li>
+    <li> Imprensa</li>
+
+</ul>   
+
+<ul id="social_networks"> 
+
+    <li id="subtitle"> REDES SOCIAIS </li>
+    <li> Instagram </li>
+    <li> Tiktok </li>
+    <li> Twitter </li>
+    <li> Facebook </li>
+    <li> Linkedln</li>
+
+</ul>  
+
+
+</footer>
+
+
+
+
+</div>
